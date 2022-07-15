c5b86a91d89364e39f11fa55180263109ab9f460
Dwadhwa95 <109305070+Dwadhwa95@users.noreply.github.com>
2022-07-15 18:25:48 +0530

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,14 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000
+monthly_payment = 2684.11
+month = 0
+total = 0
+
+while principal >0: 
+  principal = principal*(1+(0.05/12))
+  principal = principal - monthly_payment 
+  total = total + 2684.11
+  
+print(total) 
