fb481347ab4b5f443cb6ff19a991babff98fa7a6
Deepesh Wadhwa <deepesh.wadhwa95@gmail.com>
2022-07-19 19:45:11 +0530

    commit 19 July

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+total = 0
+file = open('Data/portfolio.csv', 'rt')
+next(file)
+for line in file: 
+	row = line.split(',')
+	total = total + (int(row[1])*float(row[2]))
+
+print(total)
+file.close()
